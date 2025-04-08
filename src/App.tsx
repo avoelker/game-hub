8aa40c5eef171a6f6c30aa1fce03eceee914d957
Andrew Voelker <228143+avoelker@users.noreply.github.com>
2025-04-08 12:12:04 -0700

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,27 @@
-import {
-  Stack,
-  Box,
-  Button,
-  ButtonGroup,
-  Wrap,
-  WrapItem,
-} from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
-    <Stack direction="column">
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        width="100%"
-        py={12}
-        bgImage="url('https://bit.ly/2Z4KKcF')"
-        bgPosition="center"
-        bgRepeat="no-repeat"
-        mb={2}
-      >
-        <ButtonGroup gap="4">
-          <Button colorScheme="whiteAlpha">WhiteAlpha</Button>
-          <Button colorScheme="blackAlpha">BlackAlpha</Button>
-        </ButtonGroup>
-      </Box>
-
-      <Wrap spacing={4}>
-        <WrapItem>
-          <Button colorScheme="gray">Gray</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="red">Red</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="orange">Orange</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="yellow">Yellow</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="green">Green</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="teal">Teal</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="blue">Blue</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="cyan">Cyan</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="purple">Purple</Button>
-        </WrapItem>
-        <WrapItem>
-          <Button colorScheme="pink">Pink</Button>
-        </WrapItem>
-      </Wrap>
-    </Stack>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`, // mobile
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        <NavBar></NavBar>
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 };
-
-// const App = () => {
-//   return (
-//     <HStack>
-//       <Button>Click me</Button>
-//       <Button>Click me</Button>
-//     </HStack>
-//   );
-// };
 
 export default App;
