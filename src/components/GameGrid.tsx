9a0ea8a6eb4b52046cc41eaa6d6b8de500be4b27
Andrew Voelker <228143+avoelker@users.noreply.github.com>
2025-04-08 17:07:27 -0700

    create custom hook to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,33 +1,14 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
+import useGames, { Game } from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  interface Game {
-    id: number;
-    name: string;
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <ul>
-        {games.map((game) => (
+        {games.map((game: Game) => (
           <li key={game.id}>{game.name}</li>
         ))}
       </ul>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
@@ -6,3 +6,5 @@ export default axios.create({
     key: "006c9922630d4b19ba01e69f15e0a92a",
   },
 });
+
+export { CanceledError };
