ef52c29517955f70a98e8e2b589cc940536af7d5
Andrew Voelker <228143+avoelker@users.noreply.github.com>
2025-04-10 20:39:14 -0700

    use .env file for secrets

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,7 +3,7 @@ import axios, { CanceledError } from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "006c9922630d4b19ba01e69f15e0a92a",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
 
