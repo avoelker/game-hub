3efd005f1e02467f4bdcd957cf25c7fad98170a4
Andrew Voelker <228143+avoelker@users.noreply.github.com>
2025-04-16 12:11:06 -0700

    update fetching genres and platforms using react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,6 @@
-// import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "@/services/api-client";
+import { FetchResponse } from "./useData";
 import platforms from "../data/platforms";
 
 export interface Platform {
@@ -8,6 +10,15 @@ export interface Platform {
 }
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
